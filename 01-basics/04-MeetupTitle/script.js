b32110a59fdd96b72ac38d93d8868d1f74e60cf3
n.zbiranik <n.zbiranik@cdek.ru>
2023-03-26 14:33:02 +0700

    Solve MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,22 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+  data() {
+    return {
+      limit: 5,
+      meetupId: 1,
+      meetupTitle: ''
+    }
+  },
+  watch: {
+    meetupId: {
+      immediate: true,
+      async handler(newValue, oldValue) {
+        const data = await fetchMeetupById(newValue)
+        this.meetupTitle = data.title || ''
+      },
+    }
+  }
+})
+createApp(App).mount('#app')
