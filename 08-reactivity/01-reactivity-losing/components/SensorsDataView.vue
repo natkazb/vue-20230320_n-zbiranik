68bd46fc817af6d7de02fe92468aae5357f5dab2
n.zbiranik <n.zbiranik@cdek.ru>
2023-04-19 20:32:13 +0700

    ReactivityLosing

--- 08-reactivity/01-reactivity-losing/components/SensorsDataView.vue
+++ 08-reactivity/01-reactivity-losing/components/SensorsDataView.vue
@@ -9,6 +9,7 @@
 import { SensorsDataController } from '../services/SensorsDataController';
 import { SensorsDataStreamingService } from '../services/SensorsDataStreamingService';
 import SensorsDataRow from './SensorsDataRow';
+import {klona} from "klona";
 
 export default {
   name: 'SensorsDataView',
@@ -42,7 +43,7 @@ export default {
     },
 
     setData(sensors) {
-      this.sensors = sensors;
+      this.sensors = klona(sensors);
     },
   },
 };
