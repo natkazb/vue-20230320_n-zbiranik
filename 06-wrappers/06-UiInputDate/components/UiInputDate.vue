ef6d50fddae227aa3de78432413c2617fc0e02f5
n.zbiranik <n.zbiranik@cdek.ru>
2023-04-26 21:29:24 +0700

    UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,12 @@
 <template>
-  <UiInput />
+  <UiInput :type="type" :step="step" :modelValue="value" @input="handleInput">
+    <template v-if="$slots['left-icon']" #left-icon>
+      <slot name="left-icon" />
+    </template>
+    <template v-if="$slots['right-icon']" #right-icon>
+      <slot name="right-icon" />
+    </template>
+  </UiInput>
 </template>
 
 <script>
@@ -7,7 +14,46 @@ import UiInput from './UiInput.vue';
 
 export default {
   name: 'UiInputDate',
-
   components: { UiInput },
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+      validator(value) {
+        return ['date', 'time', 'datetime-local'].includes(value)
+      }
+    },
+    modelValue: {
+      type: Number,
+      default: null,
+    },
+    step: {
+      type: [Number, String],
+    },
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    value() {
+      if (this.modelValue === null) {
+        return ''
+      }
+      const currentDate = new Date(this.modelValue)
+      const utc = new Date(currentDate.getTime() + currentDate.getTimezoneOffset() * 60000)
+      if (this.type === 'date') {
+        return currentDate.toISOString().split('T')[0]
+      } else if (this.type === 'time') {
+        return utc.toTimeString().substring(0, 5)
+      } else {
+        const datePart = currentDate.toISOString().split('T')[0]
+        const timePart = utc.toTimeString().substring(0, 5)
+        return `${datePart}T${timePart}`
+      }
+    },
+  },
+  methods: {
+    handleInput($event) {
+      this.$emit('update:modelValue', $event.target.valueAsNumber);
+    },
+  },
 };
 </script>
