d3819777b84f6cd179c867e447c3c46914060fd9
n.zbiranik <n.zbiranik@cdek.ru>
2023-05-03 21:59:52 +0700

    TemplateRenderer ???

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,30 +1,6 @@
 <script>
 import { compile, h } from 'vue';
 
-const Temp = {
-  name: 'Temp',
-
-  props: {
-    template: {
-      type: String,
-      required: true,
-    },
-    properties: {
-      type: Object,
-      default: () => ({}),
-    },
-
-    components: {
-      type: [Object, Array],
-      default: () => [],
-    },
-  },
-
-  render() {
-    return h({render: compile(this.template), components: this.components, props: {bindings: this.properties}}, {bindings: this.properties});
-  },
-};
-
 export default {
   name: 'TemplateRenderer',
 
@@ -46,6 +22,30 @@ export default {
   },
 
   render() {
+    const Temp = {
+      name: 'Temp',
+
+      props: {
+        template: {
+          type: String,
+          required: true,
+        },
+        properties: {
+          type: Object,
+          default: () => ({}),
+        },
+
+        components: {
+          type: [Object, Array],
+          default: () => [],
+        },
+      },
+
+      render() {
+        return h({render: compile(this.template), components: this.components, props: {bindings: this.properties}}, {bindings: this.properties});
+      },
+    };
+
     return h(Temp, {components: this.components, properties: this.bindings, template: this.template});
   },
 
